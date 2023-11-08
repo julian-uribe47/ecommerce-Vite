2b4857cc2b89bbb36a3c565f648e3f146d8126cd
julian-uribe47 <juribedio@hotmail.com>
2023-11-08 18:57:01 -0500

    nueva modificacion del base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'https://julian-uribe47.github.io/ecommerce-Vite/'
+  plugins: [react()]
+  
 })
